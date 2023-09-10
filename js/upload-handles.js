1495394a0565eda078cb70f54e7f56f0732ae132
朽朽朽朽朽 <694253220@qq.com>
2023-09-10 14:40:17 +0800

    修改传递参数

--- js/upload-handles.js
+++ js/upload-handles.js
@@ -1,6 +1,6 @@
 // 引入配置对象
 import configObj from './modules/config.js'
-import { transitionChangeTitle,urlFormat } from './utils.js'
+import { transitionChangeTitle, urlFormat } from './utils.js'
 
 let viewMap = {
     resourceBox: $("#resource_box"),
@@ -21,7 +21,7 @@ let viewMap = {
     placeResource(resourceHtml) {
         this.resourceBox.html(resourceHtml);
     },
-    appendResourceUrl(initUrl,cdnUrl) {
+    appendResourceUrl(initUrl, cdnUrl) {
         this.msg(`<span style="background:#9e9831;" >( 默认复制加速链接，如果是一些特殊文件加速链接可能打不开，所以在这里给出了原链~ )<span>`, true)
         this.placeResource(`
         <p style="color:#2cb144;" class="resource_box" > 
@@ -45,7 +45,7 @@ let viewMap = {
     },
     uploadSuccess({ initUrl, cdnUrl, isImage }) {
         // 将预图片去掉
-        this.sensingArea(null) 
+        this.sensingArea(null)
         let mdImg = urlFormat(cdnUrl, "md")
         let that = this;
         navigator.clipboard.writeText(isImage ? mdImg : cdnUrl).then(function () {
@@ -53,10 +53,10 @@ let viewMap = {
             that.msg(`<p id='uploadSuccessTis'>② 上传成功了，请查看剪切板！ヾ(^▽^*)))</p>`, true);
             transitionChangeTitle("success", 1000);
             if (!isImage) {
-                that.appendResourceUrl(initUrl,cdnUrl);
+                that.appendResourceUrl(initUrl, cdnUrl);
             } else {
                 // 图片回显
-                that.placeResource(`<img src="${cdnUrl}" />`) 
+                that.placeResource(`<img src="${cdnUrl}" />`)
             }
         }, function () {
             // 用户离开的页面导致复制失败，显示手动复制
@@ -66,16 +66,16 @@ let viewMap = {
             transitionChangeTitle("No way!", 500);
         });
     },
-    uploadFail({ errInfo,fileName }) {
+    uploadFail({ errInfo, fileName }) {
         if (errInfo.status == 422) {
             // 将预图片去掉
-            this.sensingArea(null) 
+            this.sensingArea(null)
             // 手动构建资源URL
             let initUrl = `https://raw.githubusercontent.com/${configObj.userAndRepo}/${configObj.branch}${configObj.path}/${new Date().Format("yyyy")}/${new Date().Format("MM")}/${new Date().Format("dd")}/${fileName}`
             let cdnUrl = cdn(initUrl)
             // 远程仓库已存在重名文件！
-            this.msg(`<p style="color:#fccd09;">② 无法上传，远程仓库已存在重名文件！</p>`,true)
-            this.appendResourceUrl(initUrl,cdnUrl);
+            this.msg(`<p style="color:#fccd09;">② 无法上传，远程仓库已存在重名文件！</p>`, true)
+            this.appendResourceUrl(initUrl, cdnUrl);
             // 给资源链接绑定copy功能
             transitionChangeTitle("ed", 1000);
         } else {
@@ -85,16 +85,16 @@ let viewMap = {
 
     },
     sensingArea(preShowURL = null) {
-      $('#img_pre_show').attr('src', preShowURL).css({
-        "display":preShowURL==null?"none":"block"
-      })
-      if(preShowURL == null) {
-        $("#upload_hint").show();
-        // 同时清空选择的文件
-        $("#myFile").val(null);
-      }else {
-        $("#upload_hint").hide();
-      }
+        $('#img_pre_show').attr('src', preShowURL).css({
+            "display": preShowURL == null ? "none" : "block"
+        })
+        if (preShowURL == null) {
+            $("#upload_hint").show();
+            // 同时清空选择的文件
+            $("#myFile").val(null);
+        } else {
+            $("#upload_hint").hide();
+        }
     }
 }
 
@@ -115,8 +115,8 @@ function fileDataProcessing(file) {
 }
 // 图片文件生成以时间缀为文件名，原后缀为后缀的fullFileName
 function genTimestampImgFileName(originalFileName) {
-    if(originalFileName == null) return originalFileName;
-    let suffix = originalFileName.substring(originalFileName.lastIndexOf(".")+1);
+    if (originalFileName == null) return originalFileName;
+    let suffix = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
     return new Date().getTime() + "." + suffix
 }
 
@@ -133,7 +133,7 @@ function cdn(url) {
 }
 
 // 主要给 UploadFromFile 使用，是UploadFromFile的支撑
-let uploadToGithub = function ( base64Data, fileName) {
+let uploadToGithub = function (base64Data, fileName) {
     if (base64Data == null) {
         resolve(null)
         return;
@@ -154,7 +154,7 @@ let uploadToGithub = function ( base64Data, fileName) {
     $.ajax({
         type: 'POST',
         // url: `https://api.github.com/repos/${configObj.userAndRepo}/contents${configObj.path}/${new Date().Format("yyyy")}/${new Date().Format("MM")}/${new Date().Format("dd")}/${fileName}`,
-        url:"https://119.91.196.91/imgUpload/",
+        url: "https://119.91.196.91/imgUpload/",
         headers: {
             // 'Authorization': 'token ' + configObj.token,
             'Content-Type': 'application/json'
@@ -162,7 +162,8 @@ let uploadToGithub = function ( base64Data, fileName) {
         data: JSON.stringify({
             // message: `Web tool: Upload ${fileName} file`,
             // branch: configObj.branch,
-            content: fileData
+            content: fileData,
+            fileName: fileName
         }),
         timeout: 0, // 设置超时时间为0，表示无限等待
         success(data) {
@@ -174,9 +175,9 @@ let uploadToGithub = function ( base64Data, fileName) {
             });
         },
         error(errInfo) {
-            viewMap.setUploadCompleted({ errInfo,fileName })
+            viewMap.setUploadCompleted({ errInfo, fileName })
         }
     })
 }
 
-export { fileDataProcessing,uploadToGithub,viewMap }
+export { fileDataProcessing, uploadToGithub, viewMap }
