9ca790bda7ba65f11f01a7b200751341fe8f6725
朽朽朽朽朽 <694253220@qq.com>
2023-09-10 13:50:27 +0800

    初版微调

---
+++ theme/index.js
@@ -0,0 +1,17 @@
+$("body").html($("body").html()+`
+    <div class="area">
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>   
+        </ul>
+    </div>
+
+`) 
